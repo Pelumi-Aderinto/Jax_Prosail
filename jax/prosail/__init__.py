9a3632dbcf1056858ea9c6724694b0c647a9d0bc
Pelumi Aderinto <64173848+Pelumi-Aderinto@users.noreply.github.com>
2025-03-15 08:56:32 +0100

    Update __init__.py

--- jax/prosail/__init__.py
+++ jax/prosail/__init__.py
@@ -9,4 +9,5 @@ from .spectral_library import get_spectra
 
 spectral_lib = get_spectra()
 from .prospect_d import run_prospect
-from .sail_model import run_prosail, run_sail, run_thermal_sail
+# from .sail_model import run_prosail, run_sail, run_thermal_sail
+from .sail_model import run_prosail_sail_jax
